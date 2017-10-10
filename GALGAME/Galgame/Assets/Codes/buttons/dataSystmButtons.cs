8a67390bf31f84942b2f40dc04c4d0f4f310b5e6
BXZR <405181821@qq.com>
2017-10-10 10:04:43 +0800

    增加systempanel
    增加windowOff按钮的级联的选项panel

--- GALGAME/Galgame/Assets/Codes/buttons/dataSystmButtons.cs
+++ GALGAME/Galgame/Assets/Codes/buttons/dataSystmButtons.cs
@@ -11,13 +11,15 @@ public class dataSystmButtons : MonoBehaviour {
 
 	public GameObject thePanelForDataSystem;//被控制显示的面板
 	public GameObject theAllController;//总控制器
+	public GameObject theChildPanel;//子panel
 	private saveLoadController theDataController;//文件存储系统的控制器
 	private thePlot thePlotController;//剧本控制器
 
 	public Sprite theAutoPicture;//自动运行的图
 	public Sprite theNotAutoPicture;//不自动运行的图
 
-	private bool isThePanelShows = false;//存档面板是不是显示了
+	private static bool isThePanelShows = false;//存档面板是不是显示了
+	private static bool isChildPanelShows = false;//有一个字panel用来存储一些不是很常用的按钮
 
 	// Use this for initialization
 	void Start () {
@@ -42,13 +44,43 @@ public class dataSystmButtons : MonoBehaviour {
 	
 	public void makeStart()
 	{
+
+		//核心是下面这两句
 		isThePanelShows = !isThePanelShows;
 		thePanelForDataSystem.gameObject.SetActive (isThePanelShows);
+		if (isThePanelShows == false) 
+		{
+			if (theChildPanel) 
+			{
+				isChildPanelShows = false;
+				theChildPanel.gameObject.SetActive (false);
+			} 
+			else 
+			{
+				//print (this.gameObject.name +" -----");
+			}
+		}
+
+	}
+
+	//回到初始界面
+	public void  backStartScene()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("start");
 	}
 
 	public void gameOver()
 	{
 		Application.Quit ();
+	}
+
+	public void  UseChildSysPanelOpen()
+	{
+		print ("isChildPanelShows = " + isChildPanelShows);
+		isChildPanelShows = !isChildPanelShows;
+		theChildPanel.gameObject.SetActive (isChildPanelShows);
+
+
 	}
 
 	//是否开启自动模式
