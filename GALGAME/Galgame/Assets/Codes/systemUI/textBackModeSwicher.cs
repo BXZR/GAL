136641d45881396d184ed71f49d6f5cbab3b5d40
BXZR <405181821@qq.com>
2017-12-07 03:08:19 +0800

    文本框风格
    可以选择普通和简洁两种风格的文本框
    不同文本框公用行不通，普通的适合看文字
    简洁的适合看CG

---
+++ GALGAME/Galgame/Assets/Codes/systemUI/textBackModeSwicher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textBackModeSwicher : MonoBehaviour {
+
+	//其实就是换一张图
+	//因为有时候文本框有点占地方，妨碍看CG，因此给出选项
+	public GameObject[] TextBackImages;
+	public Text theShowText;
+	private int indexUse = 0;
+
+	void Start () 
+	{
+		for (int i = 0; i < TextBackImages.Length; i++) 
+		{
+			TextBackImages [i].SetActive (false);
+		}
+		changeMode(0);
+		theShowText.text = "默认";
+	}
+
+
+	public void changeTextBackImageMode()
+	{
+		if (indexUse == 0) 
+		{
+			changeMode(1);
+			theShowText.text = "简洁";
+		} 
+		else if (indexUse == 1)
+		{
+			changeMode(0);
+			theShowText.text = "默认";
+		}
+	}
+
+	private void changeMode(int indexAim)
+	{
+		TextBackImages [indexUse].SetActive (false);
+		indexUse = indexAim;
+		TextBackImages [indexUse].SetActive (true);
+	}
+}
